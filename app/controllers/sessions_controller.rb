bf373ee7677ee8cf7da7c4d6613d172910f43b0f
Sangita <sangita.kreeti@example.com>
2023-07-29 12:24:51 +0530

    commit5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -52,15 +52,14 @@ class SessionsController < ApplicationController
   def find_and_authenticate_by_username_and_password
     user = User.find_by(username: params[:username])&.authenticate(params[:password])
     user ||= User.find_by(email: params[:username])&.authenticate(params[:password])
-  
+
     unless user
       flash.now[:alert] = 'Invalid username/password.'
       return nil
     end
-  
+
     user
   end
-  
 
   def session_and_cookies(user)
     cookies.encrypted[:user_id] = user.id
